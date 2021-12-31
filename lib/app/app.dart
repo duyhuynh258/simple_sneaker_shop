5e805de18e170fb7c7967a8e46b4165d215432e0
Duy Huynh <dhuynh@star.global>
2021-12-31 17:04:37 +0700

    Remove bloc and and riverpod package

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,5 @@
-// Copyright (c) 2021, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:simple_sneaker_shop/counter/counter.dart';
 import 'package:simple_sneaker_shop/l10n/l10n.dart';
 
 class App extends StatelessWidget {
@@ -27,7 +19,7 @@ class App extends StatelessWidget {
         GlobalMaterialLocalizations.delegate,
       ],
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: const SizedBox(),
     );
   }
 }
