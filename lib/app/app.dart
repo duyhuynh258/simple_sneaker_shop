6aaa3ea3b758471745c343428ee54e582e2ee3b2
Duy Huynh <dhuynh@star.global>
2022-01-03 15:18:43 +0700

    Add primary color

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:simple_sneaker_shop/core/presentation/app_colors.dart';
 import 'package:simple_sneaker_shop/l10n/l10n.dart';
 import 'package:simple_sneaker_shop/products/presentation/pages/product_page.dart';
 
@@ -11,8 +12,9 @@ class App extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: const Color(0xFF13B9FF),
+        colorScheme: const ColorScheme.light().copyWith(
+          primary: AppColors.primary,
+          secondary: AppColors.secondary,
         ),
       ),
       localizationsDelegates: const [
--- lib/core/presentation/app_colors.dart
+++ lib/core/presentation/app_colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  AppColors._();
   static const Color primary = Color(0xff788262);
   static const Color secondary = Color(0xffffffff);
 }
