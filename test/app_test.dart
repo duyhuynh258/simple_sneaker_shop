a30fda92737af96ae60a47583b979f410c3a75e6
Duy Huynh <dhuynh@star.global>
2022-01-03 14:10:42 +0700

    Update app_test

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,14 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:network_image_mock/network_image_mock.dart';
 import 'package:simple_sneaker_shop/app/app.dart';
 import 'package:simple_sneaker_shop/products/presentation/pages/product_page.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders ProductPage', (tester) async {
-      await tester.pumpWidget(const ProviderScope(child: App()));
-      await tester.pumpAndSettle();
-      expect(find.byType(ProductPage), findsOneWidget);
+      await mockNetworkImagesFor(() async {
+        await tester.pumpWidget(const ProviderScope(child: App()));
+        await tester.pumpAndSettle();
+        expect(find.byType(ProductPage), findsOneWidget);
+      });
     });
   });
 }
